416b17b4a932c402b797633921f4e1bd1303f87f
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-07-11 13:57:38 +0200

    change overlay on card

--- src/items/item.js
+++ src/items/item.js
@@ -6,11 +6,10 @@ class ItemCard extends React.Component {
   render() {
     return (
 			<Card>
-				<CardMedia
-					overlay={<CardTitle title="Magic Boots" />}
-				>
+				<CardMedia>
 					<img src="https://s-media-cache-ak0.pinimg.com/236x/79/e0/a2/79e0a2b6d9f2bb11179c094835aa1e64.jpg" />
 				</CardMedia>
+				<CardTitle title="Card title" subtitle="Card subtitle" />
 			</Card>
     );
   }
